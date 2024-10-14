0da8b08722a2e80da77b03f905834485d3a22388
Okeefegeek <92032886+Okeefegeek@users.noreply.github.com>
2024-10-14 15:12:51 -0400

    Create DoOrDontApp.swift

---
+++ DoOrDontApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoOrDontApp.swift
+//  DoOrDont
+//
+//  Created by Will O'Keefe on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DoOrDontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
